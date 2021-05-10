dba2fffac6e20b1f82c2fbf4039c2c3079624eea
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2021-05-10 17:46:50 +0200

    Implemented a prohect wide app class for main method

--- src/main/java/no/ntnu/idatg2001/postalCodes/App.java
+++ src/main/java/no/ntnu/idatg2001/postalCodes/App.java
@@ -2,6 +2,11 @@ package no.ntnu.idatg2001.postalCodes;
 
 import no.ntnu.idatg2001.postalCodes.gui.PostalCodeApplication;
 
+/**
+ * App class for the postal code registry application. Contains the main method ran by the compiler
+ * @author Sander Osvik Brekke
+ * @version 10.05.2021
+ */
 public class App {
 
     public static void main(String[] args) {
