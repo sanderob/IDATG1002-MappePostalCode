ed68f9992090e762b6bf7a0136f41abf9a897a1d
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2021-05-10 17:46:34 +0200

    Added ModuleInfo with necessary lines

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module no.ntnu.idatg2001.postalCodes{
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.base;
+    opens no.ntnu.idatg2001.postalCodes.gui;
+    opens no.ntnu.idatg2001.postalCodes.components;
+        }
